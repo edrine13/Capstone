87f5502b8cc4cfd01381d41826a88811b372e344
edrine13 <edrine.rubio@gmail.com>
2023-02-09 14:40:48 +0800

    icons

--- src/components/card/WhyUsCards.js
+++ src/components/card/WhyUsCards.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import style from "./WhyUsCards.module.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import style from './WhyUsCards.module.css';
 
 const WhyUsCards = (props) => {
   return (
@@ -8,13 +8,13 @@ const WhyUsCards = (props) => {
       className={`col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0 `}
     >
       <div
-        className={`text-center ${style["icon-box"]} ${style["card-section"]}`}
+        className={`text-center ${style['icon-box']} ${style['card-section']}`}
         data-aos="fade-up"
         data-aos-delay="100"
       >
-        <div className="icon">{props.icon}</div>
-        <h4 className={`${style.title} text-decoration-none`}>
-          <NavLink to="/">{props.title}</NavLink>
+        <div className={`${style.icon}`}>{props.icon}</div>
+        <h4 className={`${style.title} text-decoration-none my-3`}>
+          {props.title}
         </h4>
         <p className={style.description}>{props.description}</p>
       </div>
