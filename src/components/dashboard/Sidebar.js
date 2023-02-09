7c24579f6b1b5e000dcb29485ccb6499f01f7ef2
edrine13 <edrine.rubio@gmail.com>
2023-02-09 13:54:03 +0800

    icons

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -1,42 +1,52 @@
 import React from 'react';
 import style from './Sidebar.module.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFileLines } from '@fortawesome/free-solid-svg-icons';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   return (
     <aside id="sidebar" className={`${style.sidebar}`}>
-      <ul
-        className={`${style['sidebar-nav']} list-unstyled text-decoration-none`}
-        id="sidebar-nav"
-      >
-        <li className={`${style['nav-item']}`}>
-          <Link to="/user" className={`${style['nav-link']} collapsed active`}>
-            Overview
+      <ul className={`${style['sidebar-nav']}`} id="sidebar-nav">
+        <li>
+          <Link to="/user" className={`${style.link} collapsed`}>
+            <span>
+              <FontAwesomeIcon icon={faHouse} className="mx-2" />
+              Overview
+            </span>
           </Link>
         </li>
 
-        <li className={`${style['nav-item']}`}>
-          <Link to="/" className={`${style['nav-link']} collapsed`}>
+        <li>
+          <Link to="/" className={`${style.link} collapsed`}>
+            <FontAwesomeIcon icon={faUser} className="mx-2" />
             My Profile
           </Link>
         </li>
 
-        <li className={`${style['nav-item']}`}>
-          <a className={`${style['nav-link']} collapsed`} href="#">
-            <span>My Account</span>
-          </a>
+        <li>
+          <Link to="/" className={`${style.link} collapsed`}>
+            <FontAwesomeIcon icon={faFileLines} className="mx-2" />
+            My Account
+          </Link>
         </li>
 
-        <li className={`${style['nav-item']}`}>
-          <a className={`${style['nav-link']} collapsed`} href="#">
-            <span>Settings</span>
-          </a>
+        <li>
+          <Link to="/" className={`${style.link} collapsed`}>
+            <FontAwesomeIcon icon={faGear} className="mx-2" />
+            Settings
+          </Link>
         </li>
 
-        <li className={`${style['nav-item']}`}>
-          <a className={`${style['nav-link']} collapsed`} href="/">
-            <span>Sign Out</span>
-          </a>
+        <li>
+          <Link to="/" className={`${style.link} collapsed`}>
+            <FontAwesomeIcon icon={faArrowRightToBracket} className="mx-2" />
+            Sign Out
+          </Link>
         </li>
       </ul>
     </aside>
